1f1c73f231313af435b54bd5eaac2d5d9bb3a09d
prisalvarado18 <priscila.alvarado@studentambassadors.com>
2023-02-06 23:53:26 -0500

    Added React Wuery

---
+++ client/src/App.tsx
@@ -0,0 +1,29 @@
+import { trpc } from './trpc';
+import { httpBatchLink } from '@trpc/client';
+import { useState } from 'react';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import NotesList from './components/NotesList';
+
+function App() {
+  const [queryClient] = useState(() => new QueryClient());
+  const [trpcClient] = useState(() =>
+    trpc.createClient({
+      links: [
+        httpBatchLink({
+          url: 'http://localhost:3000/trpc',
+        }),
+      ],
+    })
+  );
+
+  return (
+    <trpc.Provider client={trpcClient} queryClient={queryClient}>
+      <QueryClientProvider client={queryClient}>
+        <NotesList/>
+        <div>App</div>
+      </QueryClientProvider>
+    </trpc.Provider>
+  )
+}
+
+export default App
--- src/app.ts
+++ src/app.ts
@@ -3,13 +3,16 @@ import morgan from 'morgan';
 import * as trpc from '@trpc/server';
 import * as trpcExpress from '@trpc/server/adapters/express';
 import { router, createContext } from './trpc';
+import { notesRouter } from './routes/notes';
+import cors from 'cors';
 
 const app = express();
 
 const appRouter = router({
-
+    note: notesRouter,
 })
 
+app.use(cors())
 app.use(morgan('dev'));
 
 app.use(
@@ -19,5 +22,7 @@ app.use(
         createContext,
     })
 );
+
+export type AppRouter = typeof appRouter;
 
 export default app;
