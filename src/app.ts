954414586684b272586304cdc5a1bebda32fe771
prisalvarado18 <priscila.alvarado@studentambassadors.com>
2023-02-06 21:51:33 -0500

    Added TRPC Setup

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,23 @@
 import express from 'express';
 import morgan from 'morgan';
+import * as trpc from '@trpc/server';
+import * as trpcExpress from '@trpc/server/adapters/express';
+import { router, createContext } from './trpc';
 
 const app = express();
-app.use(morgan("dev"));
+
+const appRouter = router({
+
+})
+
+app.use(morgan('dev'));
+
+app.use(
+    '/trpc',
+    trpcExpress.createExpressMiddleware({
+        router: appRouter,
+        createContext,
+    })
+);
 
 export default app;
